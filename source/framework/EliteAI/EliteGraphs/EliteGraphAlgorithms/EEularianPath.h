a7fef2eebb63680380d93e42f5f321e8282fa46e
PhaedraLambrecht <lambrechtphaedra@gmail.com>
2022-11-16 11:48:03 +0100

    Eulerian Path

--- source/framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EEularianPath.h
+++ source/framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EEularianPath.h
@@ -71,81 +71,109 @@ namespace Elite
 	template<class T_NodeType, class T_ConnectionType>
 	inline std::vector<T_NodeType*> EulerianPath<T_NodeType, T_ConnectionType>::FindPath(Eulerianity& eulerianity) const
 	{
-			// TODO: FindPath- milestone
-
 			// Get a copy of the graph because this algorithm involves removing edges
 			auto graphCopy = m_pGraph->Clone();
 			auto path = std::vector<T_NodeType*>();
 			int nrOfNodes = graphCopy->GetNrOfNodes();
 
+
 			// Check if there can be an Euler path
-			
-
-		/*	int nrOfConnections{};
-			auto CurrentNode{};
-			for (int idx{}; idx < nrOfNodes; ++idx)
-			{
-				CurrentNode = graphCopy[idx];
-			}
-			nrOfConnections = CurrentNode.GetNrOfConnections();*/
-
-
 			// If this graph is not eulerian, return the empty path
 			// Else we need to find a valid starting index for the algorithm
-			
+			T_NodeType* currentNode{ nullptr };
+
+			switch (eulerianity)
+			{
+			case Eulerianity::notEulerian:
+
+				return path;
+
+				break;
+			case Eulerianity::semiEulerian:
+
+				for (const auto& node : graphCopy->GetAllNodes())
+				{
+					const auto& connection{ graphCopy->GetNodeConnections(node) };
+						if (connection.size() % 2 == 1)
+							currentNode = node;
+
+					break;
+				}
+
+				break;
+			case Eulerianity::eulerian:
+
+				currentNode = m_pGraph->GetAllNodes()[0];
+
+				break;
+			}
+
+
 			// Start algorithm loop
 			std::stack<int> nodeStack;
 
+			// while graph size != 0  OR  nodeStack size > 0
+			while ( (graphCopy->GetNodeConnections(currentNode).size() != 0) || (nodeStack.size() > 0) )
+			{
+				// if the graph size == 0
+				if (graphCopy->GetNodeConnections(currentNode).size() == 0)
+				{
+					path.push_back(currentNode);
+					currentNode = graphCopy->GetNode(nodeStack.top());
+					nodeStack.pop();
+				}
+				else
+				{
+					nodeStack.push(currentNode->GetIndex());
+					currentNode = graphCopy->GetNode(graphCopy->GetNodeConnections(currentNode).front()->GetTo());
+					graphCopy->RemoveConnection(nodeStack.top(), currentNode->GetIndex());
+				}
 
-			std::reverse(path.begin(), path.end()); // reverses order of the path
+			}
+
+			
+
+			path.push_back(currentNode);
 			return path;
-
 	}
 
 	template<class T_NodeType, class T_ConnectionType>
 	inline void EulerianPath<T_NodeType, T_ConnectionType>::VisitAllNodesDFS(int startIdx, std::vector<bool>& visited) const
 	{
 		// mark the visited node
-
-
-		// recursively visit any valid connected nodes that were not visited before
-
-
-		/*
-				// mark the visited node
 		visited[startIdx] = true;
 
+
 		// recursively visit any valid connected nodes that were not visited before
-		for (T_ConnectionType* connection : m_pGraph->GetNodeConnections(startIdx))
-		{
+		for (const auto& connection : m_pGraph->GetNodeConnections(startIdx))
 			if (visited[connection->GetTo()] == false)
-
-
-		}
-		*/
+				VisitAllNodesDFS(connection->GetTo(), visited);
 	}
 
 		template<class T_NodeType, class T_ConnectionType>
 		inline bool EulerianPath<T_NodeType, T_ConnectionType>::IsConnected() const
 		{
-			auto nodes = m_pGraph->GetAllNodes();
+			const auto& connection{ m_pGraph->GetAllNodes() };
 			vector<bool> visited(m_pGraph->GetNrOfNodes(), false);
-
+		
 
 			// find a valid starting node that has connections
-			if (nodes.size() > 1 && m_pGraph->GetAllConnections().size() == 0)
+			if (connection.size() > 1 && m_pGraph->GetAllConnections().size() == 0)
 				return false;
 
+
 			int connectionIdx = invalid_node_index;
-			for (auto n : nodes)
+			for (const auto& node : connection)
 			{
-				auto connections = m_pGraph->GetNodeConnections(n);
+				const auto& connections = m_pGraph->GetNodeConnections(node);
 
 				if (connections.size() != 0)
 				{
-					connectionIdx = n->GetIndex();
+					connectionIdx = node->GetIndex();
 					break;
 				}
+				else
+					return false;
 			}
 
 
@@ -159,7 +187,7 @@ namespace Elite
 
 
 			// if a node was never visited, this graph is not connected
-			for (auto n : nodes)
+			for (const auto& n : connection)
 				if (visited[n->GetIndex()] == false)
 					return false;
 
